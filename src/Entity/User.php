4457e8500c1d9d1d8d18d54268c6b159af357cad
Floryss Rubechi <floryss.rubechi@gmail.com>
2022-02-03 11:45:10 +0100

    Delete some useless comments

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -70,7 +70,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->email;
+        return (string) $this->username;
     }
 
     public function getSalt(): ?string
